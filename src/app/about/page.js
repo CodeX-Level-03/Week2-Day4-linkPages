438b4bcbeb0974a61c1bc274f9059feb5dae1356
uxeeCoder <umar.afar@aol.com>
2024-04-04 23:46:42 -0400

    Added new file to link as about page

---
+++ src/app/about/page.js
@@ -0,0 +1,17 @@
+
+import React from 'react';
+import Link from 'next/link';
+export default function AboutPage() {
+    return (
+        <main>
+      <  Link href = "/">Home</Link>
+
+        <div>
+        <h1>About Page</h1>
+        <p>
+        Welcome to the About Page. This page provides information about the project. The Profile Page provides information about the user.
+        </p>
+        </div>
+        </main>
+    );
+    }
